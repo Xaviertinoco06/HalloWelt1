4fae507c1bffa4ef1c93647fa242d128acbb12f3
xavie <xavie@MSI>
2022-04-21 21:22:09 -0600

    Agregar archivos de proyecto.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//incluir libreria para salida de datos en cosola
+/*
+* 
+*/
+#include <iostream>
+
+
+int main(void) 
+{
+	std::cout << "\aHallo Welt =)\n";
+	std::cout << "1\t0\t0\t0\t0\t0\t0\t0\t0\t0\n0\t0\t0\t0";
+	std::cout << "\nLionel \"La pulga\" Messi\n";
+
+	//cout << " Hallo" << "Guten tag " << "Jijija";
+}
+
+ 
